268ecf6a8820d392720e67eb639c1003e26b8dd4
orewanf <orewanf123@gmail.com>
2015-11-14 21:24:23 +0700

    Demo

--- web/index.php
+++ web/index.php
@@ -21,5 +21,9 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
+$app->get("/cowsay", function() use($app) {
+    $app['monolog']->addDebug("cowsay");
+    return "<pre>". \League\Cowsayphp\Cow::say("cool beans") ."</pre>"
+});
 
 $app->run();
